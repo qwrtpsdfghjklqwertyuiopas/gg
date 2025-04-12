fda3ccc687f2f38ca43cdb6f672866bbde2ec66e
qwrtpsdfghjklqwertyuiopas <qwrtpsdfghjklqwertyuiopasd@gmail.com>
2025-04-13 01:05:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "LAKA-MD=8kAHXAoA#aPfobq3dzws8gbDRvuLZuTbUyLaP7NdHoxnwGI_oMFo",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=hhRSDSDZ#d4q9cT0rpqa8qIN1wWInRjkE8G74cN3ZVgVwkwL0gTc",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
